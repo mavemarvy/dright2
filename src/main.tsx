cfb3dfc1f254b11f2ef350e48fa72e1c7625d5f1
Mavemarvy <mvy00342@gmail.com>
2026-09-13 19:52:47 +0100

    Enable social mention and hashtag suggestions

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App';
 import { ThemeProvider } from './contexts/ThemeContext';
 import { CurrencyProvider } from './contexts/CurrencyContext';
 import { LanguageProvider } from './contexts/LanguageContext';
+import './lib/socialTokenAutocomplete';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
