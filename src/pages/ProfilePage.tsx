ee2411155b531b1d78dfe0759bffd5860ab4cc28
Mavemarvy <mvy00342@gmail.com>
2026-09-24 15:38:33 +0100

    Add profile balance tour anchor

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -294,6 +294,7 @@ export default function ProfilePage() {
 
       {/* Balance Card */}
       <motion.div
+        data-tour="profile-balance"
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
         className="bg-gradient-to-br from-success to-green-600 rounded-2xl p-6 text-white mb-6 shadow-lg"
